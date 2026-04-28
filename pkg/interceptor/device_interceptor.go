8866f5c0ebfd2486779446e95221580cff693471
Subhajit-Pramanik <subhop.me@gmail.com>
2026-04-28 13:34:12 +0530

    refactor: extract IP and User-Agent retrieval into separate functions in DeviceInterceptor

--- pkg/interceptor/device_interceptor.go
+++ pkg/interceptor/device_interceptor.go
@@ -17,10 +17,30 @@ func DeviceInterceptor() grpc.UnaryServerInterceptor {
 			return nil, status.Error(codes.Unauthenticated, "missing metadata")
 		}
 
-		ip := md.Get("ip_address")[0]
-		userAgent := md.Get("user_agent")[0]
+		ip := getIP(md)
+		userAgent := getUserAgent(md)
 
 		ctx = ctxutil.InjectDeviceInfo(ctx, ip, userAgent)
 		return handler(ctx, req)
 	}
+}
+
+func getIP(md metadata.MD) string {
+	ip := ""
+	if len(md.Get("x-forwarded-for")) > 0 {
+		ip = md.Get("x-forwarded-for")[0]
+	} else if len(md.Get("x-real-ip")) > 0 {
+		ip = md.Get("x-real-ip")[0]
+	}
+
+	return ip
+}
+
+func getUserAgent(md metadata.MD) string {
+	userAgent := ""
+	if len(md.Get("user-agent")) > 0 {
+		userAgent = md.Get("user-agent")[0]
+	}
+
+	return userAgent
 }
